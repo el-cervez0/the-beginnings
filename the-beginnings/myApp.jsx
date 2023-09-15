ed3537bc34e887d97bef80209dda18c4bc4435a9
Glenn Gomez <glenn.gomez61@gmail.com>
2023-09-15 14:13:43 -0500

    Adding initial files

---
+++ the-beginnings/myApp.jsx
@@ -0,0 +1,16 @@
+function MyButton() {
+    return (
+        <button>
+            Need a movie rec?
+        </button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>The Lover's Hub</h1>
+            <MovieRecButton />
+        </div>
+    );
+}
